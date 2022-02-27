d2f70432d2f0c7aa1d4d2d6a11c13e99247ea6f2
Jonathan Kamens <jik@kamens.us>
2022-02-27 15:28:29 -0500

    `make test` should actually fail on failure
    If a test fails then the test script should exit with a non-zero exit
    status.

--- test.pl
+++ test.pl
@@ -12,6 +12,24 @@ use Mail::Alias;
 $loaded = 1;
 print "ok 1\n";
 
+$exit_status = 0;
+sub good {
+  print("ok $_[0]\n");
+}
+
+sub bad {
+  print("not ok $_[0]\n");
+  $exit_status++;
+}
+
+sub check {
+  if ($_[1]) {
+    &good($_[0]);
+  }
+  else {
+    &bad($_[0]);
+  }
+}
 
 # Insert your test code below 
 
@@ -19,30 +37,26 @@ my ($alias_obj);
 
 print "Testing some of the Mail::Alias methods.........\n";
 
-if ($alias_obj = Mail::Alias->new(test_alias_file))
-	{ print "ok 2\n"} else {print "not ok 2\n"};
+&check(2, $alias_obj = Mail::Alias->new(test_alias_file));
 
 print "Setting the current alias file name\n";
 ($alias_obj->alias_file eq "test_alias_file") or die "Couldn't set the filename using MAIL::ALIAS";
 
 print "Appending some aliases to the file\n";
-if ($alias_obj->append("test_alias3", 'person1@place1.com, person2@place2.net')) 
-	{print "ok 3\n"} else {print "not ok 3\n"};
+&check(3, $alias_obj->append("test_alias3",
+                             'person1@place1.com, person2@place2.net'));
 
 print "Verifying the aliases were added\n";
-if ($alias_obj->exists("test_alias3")) 
-	{print "ok 4\n"} else {print "not ok 4\n"};
+&check(4, $alias_obj->exists("test_alias3"));
 
-if ($alias_obj->delete("test_alias3")) 
-	{print "ok 5\n"} else {print "not ok 5\n"};
+&check(5, $alias_obj->delete("test_alias3"));
 
 print "Verifying alias with continuation line at end of file\n";
-if ($alias_obj = Mail::Alias::Sendmail->new(test_alias_file))
-	{ print "ok 6\n"} else {print "not ok 6\n"};
+&check(6, $alias_obj = Mail::Alias::Sendmail->new(test_alias_file));
 
-if ($alias_obj->exists("test_alias4"))
-        {print "ok 7\n"} else {print "not ok 7\n"};
+&check(7, $alias_obj->exists("test_alias4"));
 
 print("Verifying that space after :include: works\n");
-if (($alias_obj->expand("test_alias5"))[0] eq 'included@example.com')
-        {print "ok 8\n"} else {print "not ok 8\n"};
+&check(8, ($alias_obj->expand("test_alias5"))[0] eq 'included@example.com');
+
+exit $exit_status;
